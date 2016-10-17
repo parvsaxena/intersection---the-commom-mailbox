68086f3dfb332bed9483294cea522930b2b411b1
parvsaxena <saxena.parv@gmail.com>
2016-10-17 13:12:28 +0530

    first commit

--- 4/intersection/home/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User,User_id
-
-admin.site.register(User)
-admin.site.register(User_id)
-# Register your models here.
--- 5/intersection/home/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User,User_id,Mail
-
-admin.site.register(User)
-admin.site.register(User_id)
-admin.site.register(Mail)
-# Register your models here.
